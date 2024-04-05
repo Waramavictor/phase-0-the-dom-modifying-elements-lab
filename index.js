6e92f6c91d674c0be242a059c93706dd8efa463c
waramavictor <victorkinyua16@gmail.com>
2024-04-05 18:59:43 +0300

    DOM elements lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+const yourName = 'Victor Kinyua'; 
+newHeader.innerHTML = `${yourName} is the champion`;
+
+
+document.body.appendChild(newHeader);
